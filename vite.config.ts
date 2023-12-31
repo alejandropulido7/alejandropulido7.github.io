03d642927b1adba6cbddc498a361b85f165d0335
Alejandro Pulido <hapulido22@gmail.com>
2023-12-31 00:55:03 -0500

    Adjust deploy 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "https://alejandropulido7.github.io/portfolio-v2"
 });
