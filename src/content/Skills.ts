f33117863c274e5cf9b16ab15b72347d20dee348
Alejandro Pulido <hapulido22@gmail.com>
2023-12-30 23:22:36 -0500

    Adjust deploy

--- src/content/Skills.ts
+++ src/content/Skills.ts
@@ -33,5 +33,5 @@ export const SKILLS = [
         }
 ];
 
-export const OTHER_SKILLS = ['PHP', 'Laravel', 'MongoDB', 'Figma', 'SQL', 'Typescript', 'Git', 'Docker', 'Postman', 'Python']
+export const OTHER_SKILLS = ['PHP', 'Laravel', 'MongoDB', 'Figma', 'SQL', 'Typescript', 'Git', 'Docker', 'Postman', 'Python', 'RabbitMQ']
 
