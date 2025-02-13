10923338e31301461b1478d6c6ebd2758acad7e1
Alejandro Pulido <hapulido22@gmail.com>
2025-02-13 16:36:05 -0500

    Agregar genius game project

--- src/content/Projects.ts
+++ src/content/Projects.ts
@@ -3,12 +3,25 @@ import webSunwarrior from '../assets/images/sunwarrior-page.png'
 import moduloPrestashop from '../assets/images/Modulo-prestashop.png'
 import portfolioV1 from '../assets/images/portfolio-v1.png'
 import webAppAgreements from '../assets/images/web-app-agreements.png'
+import geniusGame from '../assets/images/geniusGame.png'
 import bellAge from '../assets/images/bellAge.png'
 
 import iconGit from '../assets/images/github-svgrepo-com.svg'
 import iconEntry from '../assets/images/entry-svgrepo-com.svg'
 
 export const PROJECTS = [
+    {
+        title: 'Juego de mesa virtual',
+        description: 'En mi familia nos gusta mucho los juegos de mesa, por eso quise crear un juego interactivo donde hace consumos a apis externas para obtener peliculas, palabras aleatorias, traducciones y canciones (Con spotify API)',
+        image: geniusGame,
+        languages: ['reactJs', 'nodejs/express', 'postgres', 'jwt', 'websockets', 'APIS'],
+        links: [
+            {
+                icon: iconGit,
+                link: 'https://github.com/alejandropulido7/geniusGame'
+            },
+        ]
+    },
     {
         title: 'Web app de Contratos',
         description: 'Esta web app fue pensada para tramitar requerimientos, crear un contrato en una plantilla PDF y firmar directamente desde la app, también tiene la función de enviar por correo electronico. Tiene frontend, backend y proceso de autenticacion. Enviame un correo para entrar a probarla',
